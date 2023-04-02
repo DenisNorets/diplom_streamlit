d893496747f1607e89d408167f5c670da4843695
DenNorets <dennorets41@gmail.com>
2023-04-02 12:36:26 +0300

    test changes

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+# streamlit_app.py
+
 import streamlit as st
+import mysql.connector
+
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
 
 def demo_app():
@@ -8,8 +28,14 @@ def demo_app():
 
     if prompt:
 
-        st.write('Prompt for generating images:')
+        st.write('Prompt')
         st.write(prompt)
+
+        rows = run_query("SELECT * from application_for_calculating_the_vore;")
+
+        # Print results.
+        for row in rows:
+            st.write(f"{row[0]} has a :{row[1]}:")
 
 
 if __name__ == '__main__':
